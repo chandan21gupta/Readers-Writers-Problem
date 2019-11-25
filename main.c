ce3f4b7f76c5e613b8f992c98d7785a18411ef49
kashinkoji <chandan18386@iiitd.ac.in>
2019-11-25 23:03:45 +0530

    single reader writer working

--- main.c
+++ main.c
@@ -2,17 +2,48 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-pthread_mutex_t reader_lock;
-pthread_mutex_t writer_lock;
+pthread_mutex_t lock;
 
-pthread_t readers[4];
-pthread_t writer[4];
-int shared_data[] = {0,0,0,0,0,0,0,0};
+pthread_t reader;
+pthread_t writer;
+
+int counter = 0;
+
+void *do_read() {
+    pthread_mutex_lock(&lock);
+
+    printf("reader started\n");
+    for (unsigned long i = 0; i < (0xFFFFFFFF); i++) ;
+    printf("reader finished\n");
+
+    pthread_mutex_unlock(&lock);
+
+    return NULL;
+}
+
+void *do_write() {
+
+    pthread_mutex_lock(&lock);
+
+    printf("writer started with : %d\n",counter);
+    counter++;
+    for (unsigned long i = 0; i < (0xFFFFFFFF); i++);
+    printf("writer finished with : %d\n",counter);
+
+    pthread_mutex_unlock(&lock);
+
+    return NULL;
+}
 
 int main() {
 
-    pthread_mutex_init(&reader_lock);
-    pthread_mutex_init(&writer_lock);
-    pthread_create(&readers[0],NULL,&do_read,NULL);
-    pthread_create(&writers[0],NULL,&do_write,NULL);
+    pthread_mutex_init(&lock,NULL);
+
+    while(1){
+        pthread_create(&reader,NULL,&do_read,NULL);
+        pthread_create(&writer,NULL,&do_write,NULL);
+
+        pthread_join(writer,NULL);
+        pthread_join(reader,NULL);
+    }
 }
